ec33b4c93e185ad9a94e3688875327ea5d93440c
nadineprofesseur <16899413+nadineprofesseur@users.noreply.github.com>
2020-01-31 09:48:44 -0500

    Choix d'un visuel pour #2

---
+++ contrats.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="fr">
+<head>
+	<meta charset="utf-8">
+	<title>Liste des contrats</title>
+	<link rel="stylesheet" type="text/css" href="contrats.css">	<style>
+	</style>
+</head>
+<body>
+	<div id="header">
+		<h1><span>Contrat A Tout</span></h1>
+		<nav>
+			<div id="menu">
+				<a class="lien-page" href="#">Accueil</a> |
+				<a class="lien-page" href="#">Contrats</a> |
+				<a class="lien-page" href="#">Contacts</a>
+			</div>
+		</nav>
+	</div>
+	
+	<section id="contenu">
+		<header><h2>Contrats offerts</h2></header>
+	
+		<div id="liste-contrats">
+		
+			<div class="contrat">
+			
+				<h4>Embellissement d'un site XOOPS</h4>
+				<div class="contrat-client"><img src="illustration/profil-defaut.png"/></div>
+				<p class="contrat-description">Le site de rencontre mentor-mentoré doit être redécoré pour améliorer 
+				le taux d'inscription au projet pédagogique.</p>
+				<span class="contrat-technologie">PHP, CSS, XOOPS, AJAX</span>
+				<span class="contrat-debut"></span>
+			</div>
+		
+			<div class="contrat">
+			
+				<h4>Installation d'un site WordPress</h4>
+				<div class="contrat-client"><img src="illustration/profil-defaut.png"/></div>
+				<p class="contrat-description">Le site de rencontre mentor-mentoré doit être redécoré pour améliorer 
+				le taux d'inscription au projet pédagogique.</p>
+				<span class="contrat-technologie">PHP, CSS, XOOPS, AJAX</span>
+				<span class="contrat-debut"></span>
+			</div>
+		
+			<div class="contrat">
+			
+				<h4>Programmation d'une APP sur les océans</h4>
+				<div class="contrat-client"><img src="illustration/profil-defaut.png"/></div>
+				<p class="contrat-description">Le site de rencontre mentor-mentoré doit être redécoré pour améliorer 
+				le taux d'inscription au projet pédagogique.</p>
+				<span class="contrat-technologie">PHP, CSS, XOOPS, AJAX</span>
+				<span class="contrat-debut"></span>
+			</div>
+		
+		</div>
+	
+	
+	</section>
+	
+	<footer><span id="signature"></span></footer>
+</body>
+</html>
