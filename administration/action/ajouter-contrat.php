87f16eca7c7b0636858c977dd1da4426a9767fe0
nadineprofesseur <16899413+nadineprofesseur@users.noreply.github.com>
2020-02-03 17:37:51 -0500

    php filter + bind param dans ajouter pour #6

--- administration/action/ajouter-contrat.php
+++ administration/action/ajouter-contrat.php
@@ -1,12 +1,17 @@
 <?php
 	print_r($_POST);
-	// todo php filter
-	$titre = $_POST['titre'];
-	$client = $_POST['client'];
-	$description = $_POST['description'];
-	$technologie = $_POST['technologie'];
-	$debut = $_POST['debut'];
 	
+	$filtresContrat = 
+	array(
+		'id' => FILTER_VALIDATE_INT,
+		'titre' => FILTER_SANITIZE_ENCODED,
+		'client' => FILTER_SANITIZE_ENCODED,
+		'description' => FILTER_SANITIZE_ENCODED,
+		'technologie' => FILTER_SANITIZE_ENCODED,
+		'debut' => FILTER_SANITIZE_ENCODED
+	);
+	$contrat = filter_input_array(INPUT_POST, $filtresContrat);
+		
 	error_reporting(E_ALL);
 	ini_set('display_errors', 1);
 
@@ -17,14 +22,13 @@
 	$dsn = 'mysql:dbname='.$base.';host=' . $hote;
 	$basededonnees = new PDO($dsn, $usager, $motdepasse);
 
-	$titre = addslashes($titre); $client = addslashes($client); $description = addslashes($description); $technologie = addslashes($technologie); $debut = addslashes($debut);	// retirer avec bindparam
-	$SQL_AJOUTER_CONTRAT = "INSERT into contrat(titre, client, description, technologie, debut) VALUES('$titre', '$client', '$description', '$technologie', '$debut')";
+	$SQL_AJOUTER_CONTRAT = "INSERT into contrat(titre, client, description, technologie, debut) VALUES(:titre, :client, :description, :technologie, :debut)";
 	//echo $SQL_AJOUTER_CONTRAT;
 	$demandeAjoutContrat = $basededonnees->prepare($SQL_AJOUTER_CONTRAT);
-	//$demandeAjoutContrat->bindParam(':titre',$titre, PDO::PARAM_STR);
-	//$demandeAjoutContrat->bindParam(':client',$client, PDO::PARAM_STR);
-	//$demandeAjoutContrat->bindParam(':description',$description, PDO::PARAM_STR);
-	//$demandeAjoutContrat->bindParam(':technologie',$technologie, PDO::PARAM_STR);
-	//$demandeAjoutContrat->bindParam(':debut',$debut, PDO::PARAM_STR);
+	$demandeAjoutContrat->bindParam(':titre',$contrat['titre'], PDO::PARAM_STR);
+	$demandeAjoutContrat->bindParam(':client',$contrat['client'], PDO::PARAM_STR);
+	$demandeAjoutContrat->bindParam(':description',$contrat['description'], PDO::PARAM_STR);
+	$demandeAjoutContrat->bindParam(':technologie',$contrat['technologie'], PDO::PARAM_STR);
+	$demandeAjoutContrat->bindParam(':debut',$contrat['debut'], PDO::PARAM_STR);
 	$demandeAjoutContrat->execute();
 ?>
