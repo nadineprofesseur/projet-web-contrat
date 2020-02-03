576ec5a7ecf3c2547061c05f68190a852de49a3e
nadineprofesseur <16899413+nadineprofesseur@users.noreply.github.com>
2020-02-03 17:50:10 -0500

    php filter + bind param dans action editer pour #6

--- administration/action/editer-contrat.php
+++ administration/action/editer-contrat.php
@@ -1,12 +1,29 @@
 <?php
 	print_r($_POST);
-	// todo php filter
-	$titre = $_POST['titre'];
-	$client = $_POST['client'];
-	$description = $_POST['description'];
-	$technologie = $_POST['technologie'];
-	$debut = $_POST['debut'];
-	$id=$_GET['contrat'];
+	
+	$filtresContrat = 
+	array(
+		'id' => FILTER_VALIDATE_INT,
+		'titre' => FILTER_SANITIZE_ENCODED,
+		'client' => FILTER_SANITIZE_ENCODED,
+		'description' => FILTER_SANITIZE_ENCODED,
+		'technologie' => FILTER_SANITIZE_ENCODED,
+		'debut' => FILTER_SANITIZE_ENCODED
+	);
+	$contrat = filter_input_array(INPUT_POST, $filtresContrat);
+	
+	$filtresContrat = 
+	array(
+		'id' => FILTER_VALIDATE_INT,
+		'titre' => FILTER_SANITIZE_ENCODED,
+		'client' => FILTER_SANITIZE_ENCODED,
+		'description' => FILTER_SANITIZE_ENCODED,
+		'technologie' => FILTER_SANITIZE_ENCODED,
+		'debut' => FILTER_SANITIZE_ENCODED
+	);
+	$contrat = filter_input_array(INPUT_POST, $filtresContrat);
+	
+	$id=filter_var($_GET['contrat'],FILTER_VALIDATE_INT);
 	
 	error_reporting(E_ALL);
 	ini_set('display_errors', 1);
@@ -18,14 +35,14 @@
 	$dsn = 'mysql:dbname='.$base.';host=' . $hote;
 	$basededonnees = new PDO($dsn, $usager, $motdepasse);
 
-	$titre = addslashes($titre); $client = addslashes($client); $description = addslashes($description); $technologie = addslashes($technologie); $debut = addslashes($debut); // retirer avec bindparam
-	$SQL_EDITER_CONTRAT = "UPDATE contrat SET titre = '$titre', client = '$client', client='$description', technologie='$technologie', debut='$debut' WHERE id = '$id'";
+	$SQL_EDITER_CONTRAT = "UPDATE contrat SET titre = :titre, client = :client, client=:description, technologie=:technologie, debut=:debut WHERE id = :id";
 	echo $SQL_EDITER_CONTRAT;
 	$demandeEditionContrat = $basededonnees->prepare($SQL_EDITER_CONTRAT);
-	//$demandeEditionContrat->bindParam(':titre',$titre, PDO::PARAM_STR);
-	//$demandeEditionContrat->bindParam(':client',$client, PDO::PARAM_STR);
-	//$demandeEditionContrat->bindParam(':description',$description, PDO::PARAM_STR);
-	//$demandeEditionContrat->bindParam(':technologie',$technologie, PDO::PARAM_STR);
-	//$demandeEditionContrat->bindParam(':debut',$debut, PDO::PARAM_STR);
+	$demandeEditionContrat->bindParam(':titre',$contrat['titre'], PDO::PARAM_STR);
+	$demandeEditionContrat->bindParam(':client',$contrat['client'], PDO::PARAM_STR);
+	$demandeEditionContrat->bindParam(':description',$contrat['description'], PDO::PARAM_STR);
+	$demandeEditionContrat->bindParam(':technologie',$contrat['technologie'], PDO::PARAM_STR);
+	$demandeEditionContrat->bindParam(':debut',$contrat['debut'], PDO::PARAM_STR);
+	$demandeEditionContrat->bindParam(':id',$id, PDO::PARAM_STR);
 	$demandeEditionContrat->execute();
 ?>
