a04ffb768ec5aebc4e53b54316039db70d16e7c7
nadineprofesseur <16899413+nadineprofesseur@users.noreply.github.com>
2025-08-27 12:45:34 -0400

    Retrait formattage

--- accesseur/ContratDAO.php
+++ accesseur/ContratDAO.php
@@ -9,10 +9,10 @@
 
 		public static function initialiser()
 		{
-			$usager = 'root';
-			$motdepasse = '';
+			$usager = 'contracteur';
+			$motdepasse = 'exdyzmkKKPcM0]xO';
 			$hote = 'localhost';
-			$base = 'contrat-a-tout';
+			$base = 'contracteur';
 			$dsn = 'mysql:dbname='.$base.';host=' . $hote;
 			ContratDAO::$basededonnees = new PDO($dsn, $usager, $motdepasse);
 			ContratDAO::$basededonnees->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -52,8 +52,8 @@
 
 function formater($texte)
 {
-	$texte = html_entity_decode($texte,ENT_COMPAT,'UTF-8');
-	$texte = htmlentities($texte,ENT_COMPAT,'ISO-8859-1');
+	//$texte = html_entity_decode($texte,ENT_COMPAT,'UTF-8');
+	//$texte = htmlentities($texte,ENT_COMPAT,'ISO-8859-1');
 	return $texte;
 
 }
